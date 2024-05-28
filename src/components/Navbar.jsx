9dec3b739eabcd725df463f7cd12676a85bb6cf3
Souzinhaaz <gustavodesouzaoliveira1432@gmail.com>
2024-05-28 01:57:49 -0300

    versão final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/gsLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
@@ -8,10 +8,15 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <span className="text-4xl">Gs</span>
+        <a href="#">
+          <img width={75} src={logo} alt="Logo" />
+        </a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a target="_blank" href="https://www.linkedin.com/in/souza-undefined-706174280/">
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/souza-undefined-706174280/"
+        >
           <FaLinkedin />
         </a>
         <a target="_blank" href="https://github.com/Souzinhaaz">
