6f1051e28ac8e067dfdc7724c28eb1ae40472ed5
Souza <114781129+Souzinhaaz@users.noreply.github.com>
2024-06-04 23:30:46 -0300

    fix linkedin

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
       <div className="hidden lg:flex items-center justify-center gap-4 text-2xl">
         <a
           target="_blank"
-          href="www.linkedin.com/in/souzinhaaz"
+          href="www.linkedin.com/in/souzinhaaz/"
         >
           <FaLinkedin className="text-white hover:text-violet-300 transition transform hover:scale-105" />
         </a>
